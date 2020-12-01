b50212fdf4d7b904eb47d96c77f2ebd24a639b9d
kkgomez2 <kkristoffergomez@gmail.com>
2020-11-30 23:26:02 -0600

    Adding reel audio, changing body title styling, added background headshot gradient, eating ham

--- components/demo-reel.js
+++ components/demo-reel.js
@@ -1,10 +1,16 @@
-import styles from "./demo-reel.module.css";
-
 export default function DemoReel({ title }) {
+  let reelSrc = `demos/KevinKGomez_${title}Demo.mp3`;
   return (
-    <div className={styles.demo}>
-      <div className="text-2xl phosphate-pro-solid text-crimson-c shadow-body font-bold">{title} reel</div>
-
+    <div className="my-3 pb-3">
+      <div className="body-title">{title} reel</div>
+      <audio
+        controls
+        src={reelSrc} className="w-full">
+        <p>
+          Your browser doesn't support HTML5 audio.
+          Here is a <a href={reelSrc}>link to download the audio</a> instead.
+        </p>
+      </audio>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
 import MenuItem from "../components/menu-item";
-import DemoReel from "../components/demo-reel"
+import DemoReel from "../components/demo-reel";
 
 export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Kevin K Gomez - Voice Over</title>
+        <title>Kevin K Gomez - voice over</title>
         <link rel="icon" href="/images/favicon-96.png" />
       </Head>
 
@@ -32,104 +32,63 @@ export default function Home() {
           <div className="headshot"></div>
         </div>
 
-        <main className="w-screen flex flex-col items-center">
+        <main className="w-screen flex flex-col items-center py-4">
           <div className="w-1/2">
-            <DemoReel title="commercial">
-            </DemoReel>
+            <div id="demos" className="mb-16">
+              <DemoReel title="Commercial"></DemoReel>
 
-            <DemoReel title="character">
-            </DemoReel>
-            <h1 className="title">
-              THANK GOD{" "}
-              <Link href="/posts/first-post">
-                <a>Next.js!</a>
-              </Link>
-            </h1>
+              <DemoReel title="Character"></DemoReel>
+            </div>
 
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
-            <p className="description">
-              Get started by editing <code>pages/index.js</code>
-            </p>
+            <div id="about" className="mb-16">
+              <div className="body-title">About Me</div>
+              <div>
+                <p>
+                Kevin is represented in Chicago by Stewart Talent.
+                </p>
+                <p>
+                Voice-over actor in the Chicago area (represented by Stewart
+                Talent in the area), working in ADR, commercial, and interactive
+                work! I've got years of theater experience under my belt and a
+                mountain of training in VO, improv comedy, and film. When I'm
+                not doing all that, I can be found doodling in every margin of
+                my notebooks, playing exercise video games, and/or attempting
+                Binging with Babish recipes with moderate success.
+                </p>
+              </div>
+            </div>
+
+            <div id="resume" className="mb-16">
+              <div className="body-title">Resume</div>
+              <div>
+                Animation/ADR MARCH COMES IN LIKE A LION SHOGI PLAYER,
+                ADDITIONAL VOICES ANIPLEX OF AMERICA BEYBLADE BURST TURBO
+                ADDITIONAL VOICES DISNEY XD, SUNRIGHTS INC. EDUCATIONAL MATERIAL
+                STUDENTS, ADDITIONAL VOICES UNIVERSITY OF ILLINOIS Interactive
+                ANGELIC WAVES NATHAN BROOKS BUS ARROWS STUDIOS IN BLOOD BRENTON
+                JAIME SCRIBBLES GAMES WINDS OF CHANGE VIVIEN TAIL TAIL STUDIOS
+                EASTSHADE FEDIR EASTSHADE STUDIOS A BLOODY PARTY WILLIAM
+                MICHAELA LAWS CALADRIA CHRONICLES KYUUTAROU STARLIGHT VISUAL
+                MAGICAL WARRIOR DIAMOND HEART OPAL PYONKOTCHI THE BRAGGER’S CLUB
+                JOSEPH MICHAELA LAWS CRYING IS NOT ENOUGH THE GENERAL STORYLINE
+                TEAM OVERLOAD BAI LI REVIVAL PRODUCTIONS BLOODY CHRONICLES XEI,
+                KIMFU, DUSTIN EVANS, TERRORIST IGRASIL STUDIO Commercial IL
+                LOTTERY MARCH INSTANTS RADIO ADVERTISEMENTS WHISKEY & BANANAS
+                STUDIOS ILLINOIS CHOSE ME PROMOTIONAL MATERIAL UNIVERSITY OF
+                ILLINOIS BARCHIEF APP LAUNCH WEB ADVERTISEMENT BARCHIEF
+                Audio/Podcast PRIMORDIAL DEEP KIRAN FLINT NO SUCH THING
+                PRODUCTIONS NYM’S NEBULOUS NOTIONS TYLER RICHARDS PASSER VULPES
+                PRODUCTIONS
+              </div>
+            </div>
+
+            <div id="contact" className="mb-16">
+              <div className="body-title">Contact</div>
+            </div>
+
+            <div id="fuckyou" className="mb-16">
+              <div className="body-title">Fuck You</div>
+            </div>
           </div>
         </main>
       </div>
