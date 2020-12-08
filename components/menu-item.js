138ceb05cfa8f423d0a705bba6d5c970af195a37
kkgomez2 <kkristoffergomez@gmail.com>
2020-12-07 20:36:35 -0600

    Hey we're responsive

--- components/menu-item.js
+++ components/menu-item.js
@@ -1,3 +1,3 @@
 export default function MenuItem({ children }) {
-  return <div className="text-base py-3 text-crimson-c">{children}</div>
+  return <div className="text-base px-3 lg:px-0 lg:py-3">{children}</div>
 }
--- pages/index.js
+++ pages/index.js
@@ -15,210 +15,216 @@ export default function Home() {
         <link rel="icon" href="/images/favicon-96.png" />
       </Head>
 
-        <div className="flex flex-row justify-between">
-        <div className="headshot"></div>
+      <div className="flex flex-col-reverse lg:flex-row justify-between">
+        <div className="headshot-container">
+          <div className="headshot-image"></div>
+        </div>
 
+        <main className="flex flex-col items-center py-4 z-0">
+          <div className="w-4/5">
+            <div id="demos" className="pb-10">
+              <DemoReel title="Commercial" />
 
-          <main className="flex flex-col items-center py-4">
-            <div className="w-2/5">
-              <div id="demos" className="pb-10">
-                <DemoReel title="Commercial" />
+              <DemoReel title="Character" />
+            </div>
 
-                <DemoReel title="Character" />
-              </div>
-
-              <div id="about" className="pb-10">
-                <div className="body-title">About Me</div>
+            <div id="about" className="pb-10">
+              <div className="body-title">About Me</div>
+              <div>
                 <div>
-                  <p className="mb-5">
-                    Represented by{" "}
-                    <a href="https://www.stewarttalent.com/profile/kevin-gomez-chicago-voiceover-commercial-men">
-                      Stewart Talent
-                    </a>
-                    .
-                  </p>
-                  <p className="mb-5">
-                    Kevin K Gomez is a Chicago-based voice actor of Filipino
-                    descent with years of experience in voice over, theater,
-                    improv comedy, and film. In the realm of voice over, Kevin
-                    has specialized experience in ADR and commercial work.
-                    Clients include Aniplex of America, Sunrights Inc, and
-                    Illinois Lottery.
-                  </p>
-                  <p>
-                    When he's not doing all that, he can be found doodling in
-                    every margin of his notebooks, playing exercise video games,
-                    and/or attempting{" "}
-                    <a href="https://www.bingingwithbabish.com/recipes/spinachpuffs">
-                      Binging with Babish recipes
-                    </a>{" "}
-                    with moderate success.
-                  </p>
+                  <img
+                    className="aboutme-image mb-5"
+                    src="images/me_baby.jpg"
+                  />
                 </div>
+                <p className="mb-5">
+                  Represented by{" "}
+                  <a href="https://www.stewarttalent.com/profile/kevin-gomez-chicago-voiceover-commercial-men">
+                    Stewart Talent
+                  </a>
+                  .
+                </p>
+                <p className="mb-5">
+                  Kevin K Gomez is a Chicago-based voice actor of Filipino
+                  descent with years of experience in voice over, theater,
+                  improv comedy, and film. In the realm of voice over, Kevin has
+                  specialized experience in ADR and commercial work. Clients
+                  include Aniplex of America, Sunrights Inc, and Illinois
+                  Lottery.
+                </p>
+                <p>
+                  When he's not doing all that, he can be found doodling in
+                  every margin of his notebooks, playing exercise video games,
+                  and/or attempting{" "}
+                  <a href="https://www.bingingwithbabish.com/recipes/spinachpuffs">
+                    Binging with Babish recipes
+                  </a>{" "}
+                  with moderate success.
+                </p>
               </div>
+            </div>
 
-              <div id="resume" className="pb-10">
-                <div className="body-title">Resume</div>
-                <div>
-                  <Credits resume={AnimationResume} />
-                  <Credits resume={InteractiveResume} />
-                  <div className="font-bold text-crimson-c myriad-pro-bold text-lg">
-                    Training
-                  </div>
-                  <div className="pb-4 flex">
-                    <div className="flex flex-col w-1/3">
-                      <div className="font-bold text-crimson-c myriad-pro-bold">
-                        Animation
-                      </div>
-                      <ul>
-                        <li>Charlie Adler</li>
-                        <li>David Sobolov</li>
-                        <li>Julie Maddalena</li>
-                        <li>Tony Oliver</li>
-                        <li>Crispin Freeman</li>
-                      </ul>
-                    </div>
-                    <div className="flex flex-col w-1/3">
-                      <div className="font-bold text-crimson-c myriad-pro-bold">
-                        Commercial
-                      </div>
-                      <ul>
-                        <li>Bill Holmes</li>
-                        <li>Jeff Lupetin</li>
-                      </ul>
-                    </div>
-                    <div className="flex flex-col w-1/3">
-                      <div className="font-bold text-crimson-c myriad-pro-bold">
-                        Skills
-                      </div>
-                      <ul>
-                        <li>iO Chicago - Improv</li>
-                        <li>
-                          Kate DeVore - Dialects (RP English, Estuary English,
-                          rhotic and non-rhotic American Southern)
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="pb-8">
+            <div id="resume" className="pb-10">
+              <div className="body-title">Resume</div>
+              <div>
+                <Credits resume={AnimationResume} />
+                <Credits resume={InteractiveResume} />
+                <div className="font-bold text-crimson-c myriad-pro-bold text-lg">
+                  Training
+                </div>
+                <div className="pb-4 flex">
+                  <div className="flex flex-col w-1/3">
                     <div className="font-bold text-crimson-c myriad-pro-bold">
-                      Home Studio Spec
+                      Animation
                     </div>
                     <ul>
-                      <li>Neumann TLM 103 XLR Microphone</li>
+                      <li>Charlie Adler</li>
+                      <li>David Sobolov</li>
+                      <li>Julie Maddalena</li>
+                      <li>Tony Oliver</li>
+                      <li>Crispin Freeman</li>
+                    </ul>
+                  </div>
+                  <div className="flex flex-col w-1/3">
+                    <div className="font-bold text-crimson-c myriad-pro-bold">
+                      Commercial
+                    </div>
+                    <ul>
+                      <li>Bill Holmes</li>
+                      <li>Jeff Lupetin</li>
+                    </ul>
+                  </div>
+                  <div className="flex flex-col w-1/3">
+                    <div className="font-bold text-crimson-c myriad-pro-bold">
+                      Skills
+                    </div>
+                    <ul>
+                      <li>iO Chicago - Improv</li>
                       <li>
-                        Focusrite Scarlett Solo (2nd Gen) USB Audio Interface
-                      </li>
-                      <li>Adobe Audition CC</li>
-                      <li>
-                        Available for remote recording (Source-Connect ready).
+                        Kate DeVore - Dialects (RP English, Estuary English,
+                        rhotic and non-rhotic American Southern)
                       </li>
                     </ul>
                   </div>
-                  <div>
-                    Full resume can be found{" "}
-                    <a href="/resumes/Kevin Gomez Actors Resume (2020).pdf">
-                      here
-                    </a>
-                    .
+                </div>
+                <div className="pb-8">
+                  <div className="font-bold text-crimson-c myriad-pro-bold">
+                    Home Studio Spec
                   </div>
+                  <ul>
+                    <li>Neumann TLM 103 XLR Microphone</li>
+                    <li>
+                      Focusrite Scarlett Solo (2nd Gen) USB Audio Interface
+                    </li>
+                    <li>Adobe Audition CC</li>
+                    <li>
+                      Available for remote recording (Source-Connect ready).
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  Full resume can be found{" "}
+                  <a href="/resumes/Kevin Gomez Actors Resume (2020).pdf">
+                    here
+                  </a>
+                  .
                 </div>
               </div>
+            </div>
 
-              <div id="contact" className="pb-10">
-                <div className="body-title">Contact</div>
-                <div className="pb-4">
-                  <div className="font-bold text-crimson-c myriad-pro-bold text-lg pb-4">
-                    Representation
-                  </div>
-                  <div className="flex flex-col">
-                    <div className="w-2/3 pb-4">
-                      <a href="https://www.stewarttalent.com/" target="_blank">
-                        <img
-                          src="/images/stewart.png"
-                          title="Stewart Talent"
-                          alt="Stewart Talent"
-                        />
-                      </a>
-                    </div>
-                    <a href="mailto:vo@stewarttalent.com">
-                      vo@stewarttalent.com
-                    </a>
-                    <div>312.943.3131</div>
-                  </div>
-                </div>
-                <div className="pb-4">
-                  If you'd like to get in touch with me directly, please don't
-                  hesitate to send me an{" "}
-                  <a href="mailto:kevinkgomezvo@gmail.com">email</a>.
+            <div id="contact" className="pb-10">
+              <div className="body-title">Contact</div>
+              <div className="pb-4">
+                <div className="font-bold text-crimson-c myriad-pro-bold text-lg pb-4">
+                  Representation
                 </div>
                 <div className="flex flex-col">
-                  <div className="flex items-center pb-2">
+                  <div className="w-2/3 pb-4">
+                    <a href="https://www.stewarttalent.com/" target="_blank">
+                      <img
+                        src="/images/stewart.png"
+                        title="Stewart Talent"
+                        alt="Stewart Talent"
+                      />
+                    </a>
+                  </div>
+                  <a href="mailto:vo@stewarttalent.com">vo@stewarttalent.com</a>
+                  <div>312.943.3131</div>
+                </div>
+              </div>
+              <div className="pb-4">
+                If you'd like to get in touch with me directly, please don't
+                hesitate to send me an{" "}
+                <a href="mailto:kevinkgomezvo@gmail.com">email</a>.
+              </div>
+              <div className="flex flex-col">
+                <div className="flex items-center pb-2">
+                  <a href="mailto:kevinkgomezvo@gmail.com">
+                    <img
+                      src="/images/icons/iconfinder_email-letter-inbox-send_2203552.svg"
+                      className="fill-crimson w-6"
+                      title="email"
+                      alt="email icon"
+                    ></img>
+                  </a>
+                  <div className="pl-3">
                     <a href="mailto:kevinkgomezvo@gmail.com">
-                      <img
-                        src="/images/icons/iconfinder_email-letter-inbox-send_2203552.svg"
-                        className="fill-crimson w-6"
-                        title="email"
-                        alt="email icon"
-                      ></img>
+                      kevinkgomezvo@gmail.com
                     </a>
-                    <div className="pl-3">
-                      <a href="mailto:kevinkgomezvo@gmail.com">
-                        kevinkgomezvo@gmail.com
-                      </a>
-                    </div>
                   </div>
-                  <div className="flex items-center pb-2">
+                </div>
+                <div className="flex items-center pb-2">
+                  <a href="https://twitter.com/kevinkgomez" target="_blank">
+                    <img
+                      src="/images/icons/iconfinder_BW_Twitter_glyph_svg_5305170.svg"
+                      className="fill-crimson w-6"
+                      title="Twitter"
+                      alt="Twitter icon"
+                    ></img>
+                  </a>
+                  <div className="pl-3">
                     <a href="https://twitter.com/kevinkgomez" target="_blank">
-                      <img
-                        src="/images/icons/iconfinder_BW_Twitter_glyph_svg_5305170.svg"
-                        className="fill-crimson w-6"
-                        title="Twitter"
-                        alt="Twitter icon"
-                      ></img>
+                      @kevinkgomez
                     </a>
-                    <div className="pl-3">
-                      <a href="https://twitter.com/kevinkgomez" target="_blank">
-                        @kevinkgomez
-                      </a>
-                    </div>
                   </div>
-                  <div className="flex items-center pb-2">
+                </div>
+                <div className="flex items-center pb-2">
+                  <a
+                    href="https://instagram.com/kevinkgomez.mp3"
+                    target="_blank"
+                  >
+                    <img
+                      src="/images/icons/iconfinder_Instagram_glyph_svg_5335781.svg"
+                      className="fill-crimson w-6"
+                      title="Instagram"
+                      alt="Instagram icon"
+                    ></img>
+                  </a>
+                  <div className="pl-3">
                     <a
                       href="https://instagram.com/kevinkgomez.mp3"
                       target="_blank"
                     >
-                      <img
-                        src="/images/icons/iconfinder_Instagram_glyph_svg_5335781.svg"
-                        className="fill-crimson w-6"
-                        title="Instagram"
-                        alt="Instagram icon"
-                      ></img>
+                      kevinkgomez.mp3
                     </a>
-                    <div className="pl-3">
-                      <a
-                        href="https://instagram.com/kevinkgomez.mp3"
-                        target="_blank"
-                      >
-                        kevinkgomez.mp3
-                      </a>
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </main>
+          </div>
+        </main>
+        <div className="header-container">
           <div className="header">
-            <div className="flex-col p-6 z-10">
+            <div className="flex flex-row justify-between lg:flex-col p-6 z-10">
               <div className="shadow-title text-white pb-4">
-                <div className="text-6xl font-black phosphate-pro-solid leading-12.5">
+                <div className="text-2xl sm:text-6xl sm:leading-12.5 phosphate-pro-solid ">
                   Kevin K <br /> Gomez
                 </div>
-                <div className="myriad-pro-bold text-3xl tracking-even-widest -mt-1">
+                <div className="text-md sm:text-3xl tracking-even-widest myriad-pro-bold  -mt-1">
                   voice over
                 </div>
               </div>
-              <div className="menu flex-col">
+              <div className="menu flex-row lg:flex-col">
                 <MenuItem>
                   <a href="#demos">Demos</a>
                 </MenuItem>
@@ -235,7 +241,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-
+      </div>
     </div>
   );
 }
